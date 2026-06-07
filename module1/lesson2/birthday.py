3a6512a05c745ab80514ff7bc6276115ae8418bf
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-06-07 11:54:18 +0530

    birthday project

---
+++ module1/lesson2/birthday.py
@@ -0,0 +1,22 @@
+# Making a project where different birthday dates are stored in variables (5).
+Mine="10th October"
+
+My_mother="10th October"
+
+My_father="25th May"
+
+My_bestfriend="23rd April"
+
+My_sister="2nd April"
+
+# Printing the dates in five different print statements.
+
+print("My birthday is", Mine)
+
+print("My mother's birthday is", My_mother)
+
+print("My father's birthday is", My_father)
+
+print("My bestfriend's birthday is", My_bestfriend)
+
+print("My sister's birthday is ", My_sister)
