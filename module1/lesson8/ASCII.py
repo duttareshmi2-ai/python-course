2214af0cd38d18e154213621a566736af9ad83ac
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-06-26 10:30:33 +0530

    ASCII

--- module1/lesson8/ASCII.py
+++ module1/lesson8/ASCII.py
@@ -1,2 +1,12 @@
-take=int(input("Enter any numerical code: "))
-out=print(bin(take))
+take=input("Enter anything: ")
+if take.islower:
+    print("Input is in lowercase fully.")
+elif take.isupper:
+    print("Input is in uppercase fully.")
+else:
+    print("Input is mixed up of uppercase and lowercase.")
+if len(take)==1:
+    out=ord(take)
+    print("Unicode", out)
+else:
+    print("Enter only one special caracter.")
