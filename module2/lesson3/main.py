d78c253d90f9bdae3c1a9caca886674a8ea75152
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-07-11 10:04:15 +0530

    tic tac toe

---
+++ module2/lesson3/main.py
@@ -0,0 +1,18 @@
+from rich import Console
+# Rule-based Chatbots: 
+# How they work: 
+# 1. Predefined Rules: Use a set of hard-coded rules and patterns to respond to user inputs 
+# 2. Pattern Matching: Identify keywords or phrases in user input to trigger specific responses. 
+# 3. Decision Trees: Use nested conditional statements to handle complex conversations flow. 
+# Advanced Rule-based Chatbots: 1. Error Handling: Providing meaningful messages when chatbot dosen't understand the user. 2. State Management: Keep the track of conversation context to provide context-aware responses. 3.Modular Design: Break downs the chatbot's functionality into module(e.g. greeting,help,FAQ's)
+
+# NLP : Natural Language Processing (NLP) it's a branch of AI that helps computers understand, interpret, and generate human language.
+# Importance: Understanding user intent and Handling Variations in Language.
+# NLP in chatbots: 1. Understanding variations 2.Contextual Responses 3. Handling Ambiguity.
+
+
+# Activity : Create an rule-based chatbot using Python.
+c=Console()
+c.print("Hi! My name is dumbai. ", style="orange")
+c.print("-I can help you with packaging (tips for trips)", style="yellow")
+c.print("-I can ")
