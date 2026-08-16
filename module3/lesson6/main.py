a4fcdcd44d5c3bc2e509fbcd6842041f5bdb395b
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-08-16 10:48:01 +0530

    Math and Random module

---
+++ module3/lesson6/main.py
@@ -0,0 +1,27 @@
+# Random Module : It's a built in Python tootlkit which helps us in generating pseudo random numbers. 
+import random
+# print(random.random())
+# print(str(random.randint(0,45)))
+# var=["hello","world","python"]
+# print(random.choice(var))
+# number=random.randint(1,3)
+# if number==1:
+#     print("The number is 1.")
+# Math Module : It's a built in Python toolkit which helps us with calculations that go beyond basic arithmetic operations like rounding, float and absolute values, and finding the Greatest Common Divisor any numbers more than one number.
+import math
+# lcm=math.lcm()
+# # print(lcm)
+# print(math.ceil(4.2))
+# print(math.floor(4.8))
+# print(math.copysign(-3.8,-5))
+# print(math.fabs(-7.5))
+# Activity 1 : Number game
+# Outline:
+# Write a program to generate a random integer and match it with the input given by the user?
+# while True:
+#     num=random.randint(1,10)
+#     user=int(input("Guess the number : "))
+#     if user==num:
+#         print("Correct!")
+#     else:
+#         print("Wrong")
