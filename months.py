894eb1c73ef3d35555094189cee5b42c78819a63
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-08-30 15:50:13 +0530

    Display of All months

---
+++ months.py
@@ -0,0 +1,11 @@
+# Program to display all months' names
+
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+
+print("List of Months:")
+for m in months:
+    print(m)
