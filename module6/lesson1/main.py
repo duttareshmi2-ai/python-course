7861f72252c07bffb80db34f6d92fec4dd5b6a08
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-09-06 10:04:11 +0530

    class

--- module6/lesson1/main.py
+++ module6/lesson1/main.py
@@ -66,3 +66,11 @@ print("---------------------------------")
 # Activity 3  : Class Parrot
 # Outline:
 # Write a program to create a class Parrot and perform the following tasks - 1. Create a class variable species 2. Create a __init__ method that has instance variables - name and age 3. Create instances of class Parrot, passing arguments as well 4. Print Class variable by accessing it 5. Print Instance variables as well
+class Parrot:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def ret(self):
+        return f"This parrot's name is {self.name} and it's age is {self.age}"
+parrot1=Parrot("Ricky",9).ret()
+print(parrot1)
