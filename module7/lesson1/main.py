3216e96fa8b5e240193450e4cd6cfc63d5db6163
duttareshmi2-ai <duttareshmi2@gmail.com>
2026-09-19 10:45:45 +0530

    Pygame

--- module7/lesson1/main.py
+++ module7/lesson1/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 pg.init()
 screen = pg.display.set_mode((1000,500))
+img1 = pg.transform.scale(pg.image.load("smiley.bmp").convert_alpha() , (200,200))
 screen.fill((255 , 255 , 255))
 pg.display.set_caption("FIRST")
 clock = pg.Clock()
@@ -12,5 +13,6 @@ while True :
     circle = pg.draw.circle(screen , (255,255,0) , (500 , 250) , 30)
     text = pg.font.Font("DS-DIGIT.ttf", 33).render("This is a Circle" , True , pg.Color("black"))
     screen.blit(text , (420  , 300))
+    screen.blit(img1 , (0,0))
     pg.display.flip()
     clock.tick(60)
